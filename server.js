09276c18a86402dd114eab16e595749e7060f1b6
Roshan Dalami <tech.channel.002@gmail.com>
2021-09-22 22:58:33 +0530

    databaseProblemSolvedByChange

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 //Setting up view engines
 app.set('view engine','ejs')
@@ -23,7 +23,10 @@ app.use('/',indexRouter)
 app.use('/authors',authorRouter)
 
 //connecting to the dataBase
-mongoose.connect(process.env.DATABASE_URL).then(()=>console.log('DataBase Connected Successfuly')).catch((error)=>console.log(error))
+
+//  const db = require('./setup/mykeys').myKey;   
+ const db = process.env.DATABASE_URL
+mongoose.connect(db).then(()=>console.log('DataBase Connected Successfuly')).catch((error)=>console.log('fuck error'))
 
 app.listen(PORT,()=>{
     console.log(`Server is running ${PORT}`)
